c252112aa5fadc05ed2c440469d18620aa990165
LeeladharRao <kompallyleeladhar@gmail.com>
2021-03-17 22:49:50 +0530

    Created paths to pages

--- app.py
+++ app.py
@@ -2,9 +2,20 @@ from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
 @app.route("/")
-@app.route("/home")
 def home():
-    return render_template('index.html')
+    return render_template('home.html')
+
+@app.route("/about")
+def about():
+    return render_template('about.html')
+
+@app.route("/portfolio")
+def portfolio():
+    return render_template('portfolio.html')
+
+@app.route("/blog")
+def blog():
+    return render_template('blog.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
